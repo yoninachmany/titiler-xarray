9dc492a6fd57fe1473144eb889d070a9a91af41f
Yoni Nachmany <yoni.nachmany@gro-intelligence.com>
2024-01-24 14:58:24 -0500

    Setup Arraylake in __init__.py

--- titiler/xarray/__init__.py
+++ titiler/xarray/__init__.py
@@ -1,3 +1,11 @@
 """titiler.xarray"""
 
 __version__ = "0.1.0"
+
+import logging
+import os
+
+os.environ["ZARR_V3_EXPERIMENTAL_API"] = "1"
+
+# the httpx library (an Arraylake dependency) somehow comes through with debug logging
+logging.getLogger("httpx").setLevel(logging.WARNING)
